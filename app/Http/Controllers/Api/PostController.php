7d52f1eb75bbb3bfb1508e1b7801c695b349859f
fennich-soufiane-fs <fennich.soufiane.fs@gmail.com>
2025-03-14 15:34:59 +0000

    Apply try catch

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Post;
 use App\Http\Requests\CreatePostRequest;
+use Exception;
+
 
 class PostController extends Controller
 {
@@ -14,15 +16,19 @@ class PostController extends Controller
     }
 
     public function store(CreatePostRequest $request) {
-        $post = new Post();
-        $post->titre = $request->titre;
-        $post->description = $request->description;
-        $post->save();
+        try {
+            $post = new Post();
+            $post->titre = $request->titre;
+            $post->description = $request->description;
+            $post->save();
 
-        return response()->json([
-            'status_code' => 200,
-            'status_message' => 'Posts Created Successfully',
-            'data' => $post,
-        ]);
+            return response()->json([
+                'status_code' => 200,
+                'status_message' => 'Posts Created Successfully',
+                'data' => $post,
+            ]);
+        } catch(Exeption $e) {
+            return response()->json($e);
+        }
     }
 }
