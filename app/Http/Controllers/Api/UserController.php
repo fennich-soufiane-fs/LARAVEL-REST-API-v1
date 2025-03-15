66ff02d18a5befd1875ae327953aef6383bbcc1d
fennich-soufiane-fs <fennich.soufiane.fs@gmail.com>
2025-03-15 15:04:06 +0000

    Return a Response After Registration

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -7,20 +7,37 @@ use Illuminate\Http\Request;
 use App\Http\Requests\RegisterRequestUser;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
+use Exception;
 
 
 
 class UserController extends Controller
 {
     public function register(RegisterRequestUser $request) {
-        $user = new User();
-        
-        $user->name = $request->name;
-        $user->email = $request->email;
-        $user->password = Hash::make($request->password, [
-            'rounds' => 12
-        ]);
 
-        $user->save();
+        try {
+            $user = new User();
+        
+            $user->name = $request->name;
+            $user->email = $request->email;
+            $user->password = Hash::make($request->password, [
+                'rounds' => 12
+            ]);
+
+            $user->save();
+            return response()->json([
+                'status_code' => 200,
+                'status_message' => 'User registred successfully',
+                'user' => $user
+            ]);
+        } catch (Exception $e) {
+        return response()->json([
+            'status_code' => 500,
+            'status_message' => 'An error occurred.',
+            'error' => $e->getMessage()
+        ], 500);
+
+        
+        }
     }
 }
